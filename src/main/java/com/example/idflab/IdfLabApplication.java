46f15fbb465c9eafd0abe3df4975e273d6e7c674
FIRST_NAME LAST_NAME <MY_NAME@example.com>
2022-07-09 21:42:49 +0300

    Initial commit

---
+++ src/main/java/com/example/idflab/IdfLabApplication.java
@@ -0,0 +1,13 @@
+package com.example.idflab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IdfLabApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IdfLabApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/idflab/IdfLabApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.idflab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IdfLabApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
